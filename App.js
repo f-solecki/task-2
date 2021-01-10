7e8b6283b8dd08a2194f39aac128de34ff000d31
soolecki <filipsolecki@gmail.com>
2021-01-10 22:35:17 +0100

    Added some styles to components

--- App.js
+++ App.js
@@ -6,9 +6,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Button title='I am the button, press me!' onPress={() => Alert.alert("You pressed the button")} />
-      <TouchableOpacity onPress={() => Alert.alert('You pressed the TouchableOpacity')}><Text>I am TouchableOpacity with Text inside, Press me!</Text></TouchableOpacity>
-      <TouchableWithoutFeedback onPress={() => Alert.alert('You pressed TouchableWithoutFeedback')}><Text>I am TouchableWithoutFeedback, Press me!</Text></TouchableWithoutFeedback>
-      <Pressable onPress={() => Alert.alert('You pressed Pressable')}><Text>I am Pressable, Press me!</Text></Pressable>
+      <TouchableOpacity style={styles.touchable} onPress={() => Alert.alert('You pressed the TouchableOpacity')}><Text>I am TouchableOpacity with Text inside, Press me!</Text></TouchableOpacity>
+      <TouchableWithoutFeedback style={styles.touchableWithoutFeedback} onPress={() => Alert.alert('You pressed TouchableWithoutFeedback')}><Text>I am TouchableWithoutFeedback, Press me!</Text></TouchableWithoutFeedback>
+      <Pressable style={styles.pressable} onPress={() => Alert.alert('You pressed Pressable')}><Text>I am Pressable, Press me!</Text></Pressable>
     </View>
   );
 }
@@ -16,9 +16,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff0',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'space-around',
+  },
+  touchable: {
+    backgroundColor: '#ccc',
+    padding: 5
+  },
+  touchableWithoutFeedback: {
+    backgroundColor: '#bbb',
+    padding: 5
+  },
+  pressable: {
+    backgroundColor: '#aaa',
+    padding: 5
   },
 });
 
